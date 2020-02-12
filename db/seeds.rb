71921ea02abcd872e879f007dde685df24310c1b
Lea Medrano <leamedrano@gmail.com>
2020-02-12 11:38:27 +0100

    created seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 10 fake restaurants...'
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Movies::HarryPotter.character,
+    address: Faker::Movies::HarryPotter.location,
+    phone_number: Faker::Number.leading_zero_number(digits: 10),
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  )
+  restaurant.save!
+end
+puts 'Finished!'
