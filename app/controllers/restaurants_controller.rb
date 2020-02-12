295d6fb1df0d1059e0ced385f405401ce3b987d6
Lea Medrano <leamedrano@gmail.com>
2020-02-12 12:24:51 +0100

    set controllers restaurants and reviews

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,22 +1,28 @@
 class RestaurantsController < ApplicationController
   def index
+    @restaurants = Restaurant.all
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
-  def edit
-  end
+  private
 
-  def update
-  end
-
-  def destroy
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,22 +1,23 @@
 class ReviewsController < ApplicationController
-  def index
-  end
-
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
-  def show
-  end
+  private
 
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
